5ba3e31fd52276c295ebcd7a26bb2f1b8e34673f
Yohanes Dwihastana <yohansatriani@yahoo.co.id>
2018-10-22 06:37:38 +0700

    login fix

--- falconet/views.py
+++ falconet/views.py
@@ -4,16 +4,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from falconet.forms import LoginForm
 
 @login_required()
 def home(request):
-    html = render_to_string('page-home.html', {'title': "Home", 'head': "Home"})
+    html = render_to_string('page-home.html', {'title': "Home", 'head': "Home", 'bcitems': ['home']})
     return HttpResponse(html)
 
 @login_required()
 def sites(request):
-    html = render_to_string('page-sites.html', {'title': "Sites", 'head': "Sites"})
+    html = render_to_string('page-sites.html', {'title': "Sites", 'head': "Sites", 'bcitems': ['home', 'sites']})
     return HttpResponse(html)
 
 def auth_login(request):
@@ -31,11 +32,16 @@ def auth_process(request):
     if request.method == 'POST':    
         username = request.POST['username']
         password = request.POST['password']
+        next_url = request.POST['next']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect(home)
+            if next_url:
+                return redirect(next_url)
+            else:
+                return redirect(home)
         else:
+            messages.error(request,'Authentication Failed: Username or password is incorrect')
             return redirect(auth_login)
     else:
         if not request.user.is_authenticated:
