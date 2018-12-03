84ad7206db536a202d62044adee5f68feb1f619b
Apple <apple@TI-QA-MAC.local>
2018-12-03 18:21:42 +0700

    link count

--- netinfo/views.py
+++ netinfo/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.contrib import messages
 
 from falconet import views
@@ -36,9 +37,16 @@ def edit_data(site_post_data, site_db_data):
 @login_required()
 def sites(request):
     site_data = sites_model.objects.all()
+
+    link_count = []
+    for site in site_data:
+        link_count.append(links_model.objects.filter(Q(sites1=site.id)|Q(sites2=site.id)).count())
+
+    pprint(site_data)
+    pprint(link_count)
     # breadcrumbs
     bcitems = [['/home/', 'Home'], ['/sites/', 'Sites']]
-    return render(request, "page-sites.html", {'title': "Sites", 'head': "Sites", 'bcitems': bcitems, 'site_data': site_data})
+    return render(request, "page-sites.html", {'title': "Sites", 'head': "Sites", 'bcitems': bcitems, 'site_data': site_data, 'link_count': link_count})
 
 @login_required()
 def site_office(request):
@@ -62,11 +70,15 @@ def site_detail(request, site_id):
         site_id = int(site_id)
     except ValueError:
         raise Http404()
+    #site info
     site_data = get_object_or_404(sites_model, id=site_id)
+    #contact info
     contacts_data = contacts_model.objects.filter(site = site_id)
+    #link info
+    links_data = links_model.objects.filter(Q(sites1=site_id)|Q(sites2=site_id))
     # breadcrumbs
     bcitems = [['/home/', 'Home'], ['/sites/', 'Sites'], [site_id, site_data.name]]
-    return render(request, "page-site-detail.html", {'title': "Sites", 'head': "Sites", 'bcitems': bcitems, 'site_data': site_data ,'contacts_data': contacts_data})
+    return render(request, "page-site-detail.html", {'title': "Sites", 'head': "Sites", 'bcitems': bcitems, 'site_data': site_data , 'contacts_data': contacts_data, 'links_data': links_data})
 
 @login_required()
 def site_detail_edit(request, site_id):
@@ -385,113 +397,65 @@ def link_detail(request, link_id):
 def link_detail_edit(request, link_id):
     if request.method == 'POST':
         pprint(request.POST)
-    #     # site post intialization
-    #     site_id = int(request.POST['id'])
-    #     # contact data & type
-    #     contacts_type = contacts_model.objects.values('type').distinct()
-    #     contacts_data = contacts_model.objects.filter(site = site_id)
-    #
-    #     site_post_data = {
-    #         'id': request.POST['id'],
-    #         'name':request.POST['name'],
-    #         'description':request.POST['description'],
-    #         'type':request.POST['type'],
-    #         'location':request.POST['location'],
-    #         'city':request.POST['city'],
-    #         'site_code':request.POST['site_code'],
-    #         'area_code':request.POST['area_code'],
-    #         'ipadd':request.POST['ipadd'],
-    #         'tagline':request.POST['tagline']
-    #     }
-    #
-    #     site_data = sites_model.objects.get(id=int(request.POST['id']))
-    #
-    #     site_db_data = {
-    #         'id': site_data.id,
-    #         'name': site_data.name,
-    #         'description':site_data.description,
-    #         'type': site_data.type,
-    #         'location': site_data.location,
-    #         'city': site_data.city,
-    #         'site_code': site_data.site_code,
-    #         'area_code': site_data.area_code,
-    #         'ipadd': site_data.ipadd,
-    #         'tagline': site_data.tagline,
-    #     }
-    #
-    #     # contact post update process
-    #     if 'contact_id' in request.POST:
-    #         contacts_post_data_raw = [
-    #             request.POST.getlist('contact_id'),
-    #             request.POST.getlist('contact_type'),
-    #             request.POST.getlist('contact_number'),
-    #         ]
-    #         # transpose post data
-    #         contacts_post_data_zip=list(map(list, zip(*contacts_post_data_raw)))
-    #
-    #         for contacts_post_data_list in contacts_post_data_zip:
-    #             contacts_data = contacts_model.objects.get(id=contacts_post_data_list[0])
-    #             contacts_db_data = {
-    #                 'contact_id': contacts_data.id,
-    #                 'contact_type': contacts_data.type,
-    #                 'contact_number':contacts_data.contact_number,
-    #             }
-    #             contacts_post_data = {
-    #                 'contact_id': contacts_post_data_list[0],
-    #                 'contact_type': contacts_post_data_list[1],
-    #                 'contact_number': contacts_post_data_list[2],
-    #             }
-    #             contact_form = ContactForm(contacts_post_data, initial=contacts_db_data)
-    #             if contact_form.is_valid():
-    #                 if contact_form.has_changed():
-    #                     for changed_data in contact_form.changed_data:
-    #                         setattr(contacts_data, changed_data, contacts_post_data[changed_data])
-    #                         contacts_data.save()
-    #                     messages.success(request, "Contact: "+contacts_post_data['contact_type'].title()+":"+contacts_post_data['contact_number']+" updated succesfully.", extra_tags='alert-success')
-    #                 else:
-    #                     messages.info(request, "Contact: "+contacts_post_data['contact_type'].title()+":"+contacts_post_data['contact_number']+" not changed.", extra_tags='alert-info')
-    #             else:
-    #                 messages.error(request, 'Failed updating contact'+contacts_post_data['contact_type'].title()+":"+contacts_post_data['contact_number'], extra_tags='alert-danger')
-    #
-    #     # contact post delete process
-    #     if 'del_contact_id' in request.POST:
-    #         contacts_post_del_id = request.POST.getlist('del_contact_id')
-    #
-    #         for del_id in contacts_post_del_id:
-    #             contacts_data = contacts_model.objects.get(id=del_id)
-    #             messages.success(request, "Contact: "+contacts_data.type+":"+contacts_data.contact_number+" deleted succesfully.", extra_tags='alert-success')
-    #             contacts_data.delete()
-    #
-    #     # contact post add process
-    #     if 'add_contact_id' in request.POST:
-    #         contacts_post_add_dataraw = [
-    #             request.POST.getlist('add_contact_type'),
-    #             request.POST.getlist('add_contact_number'),
-    #         ]
-    #
-    #         contacts_post_add_data=list(map(list, zip(*contacts_post_add_dataraw)))
-    #
-    #         for contacts_post_add in contacts_post_add_data:
-    #             contacts_model(site=sites_model.objects.get(id=site_id), type=contacts_post_add[0], contact_number=contacts_post_add[1]).save()
-    #             messages.success(request, "Contact: "+contacts_post_add[0]+":"+contacts_post_add[1]+" added succesfully.", extra_tags='alert-success')
-    #
-    #     # site update process
-    #     site_form = SiteForm(site_post_data, initial=site_db_data)
-    #     if site_form.is_valid():
-    #         if site_form.has_changed():
-    #             for changed_data in site_form.changed_data:
-    #                 setattr(site_data, changed_data, site_post_data[changed_data])
-    #                 site_data.save()
-    #             messages.success(request, 'Site Information updated succesfully.', extra_tags='alert-success')
-    #             return redirect('site_detail', site_id=site_post_data['id'])
-    #         else:
-    #             messages.info(request, 'Site Information not changed.', extra_tags='alert-info')
-    #             return redirect('site_detail', site_id=site_post_data['id'])
-    #     else:
-    #         messages.error(request, 'Failed updating Site Information.', extra_tags='alert-danger')
-    #         # breadcrumbs
-    #         bcitems = [['/home/', 'Home'], ['/sites/', 'Sites'], ['/sites/'+str(site_id)+'/', site_data.name],['edit', 'Edit']]
-    #         return render(request, 'page-site-detail-edit.html', {'title': "Edit Sites", 'head': "Edit Sites", 'bcitems': bcitems, 'contacts_data': contacts_data, 'contacts_type': contacts_type, 'site_form': site_form, 'site_id': site_id})
+        # site post intialization
+        link_id = int(request.POST['id'])
+
+        link_post_data = {
+            'id': request.POST['id'],
+            'sites1':request.POST['sites1'],
+            'sites2':request.POST['sites2'],
+            'ipadd1':request.POST['ipadd1'],
+            'ipadd2':request.POST['ipadd2'],
+            'isp':request.POST['isp'],
+            'bandwidth':request.POST['bandwidth'],
+            'media':request.POST['media'],
+            'services':request.POST['services'],
+            'status':request.POST['status'],
+            'ipadd_others':request.POST['ipadd_others'],
+            'vrf_name':request.POST['vrf_name'],
+            'links_name':request.POST['links_name'],
+            'isp_link_id':request.POST['isp_link_id'],
+            'input_date':request.POST['input_date'],
+        }
+
+        link_data = links_model.objects.get(id=link_id)
+
+        link_db_data = {
+            'id': link_data.id,
+            'sites1':link_data.sites1,
+            'sites2':link_data.sites2,
+            'ipadd1':link_data.ipadd1,
+            'ipadd2':link_data.ipadd2,
+            'isp':link_data.isp,
+            'bandwidth':link_data.bandwidth,
+            'media':link_data.media,
+            'services':link_data.services,
+            'status':link_data.status,
+            'ipadd_others':link_data.ipadd_others,
+            'vrf_name':link_data.vrf_name,
+            'links_name':link_data.links_name,
+            'isp_link_id':link_data.isp_link_id,
+            'input_date':link_data.input_date,
+        }
+        pprint(link_post_data)
+        pprint(link_db_data)
+        # link update process
+        link_form = LinkForm(link_post_data, initial=link_db_data)
+        if link_form.is_valid():
+            if link_form.has_changed():
+                for changed_data in link_form.changed_data:
+                    setattr(link_data, changed_data, link_post_data[changed_data])
+                    link_data.save()
+                messages.success(request, 'Link data updated succesfully.', extra_tags='alert-success')
+                return redirect('link_detail', link_id=link_post_data['id'])
+            else:
+                messages.info(request, 'Link data not changed.', extra_tags='alert-info')
+                return redirect('link_detail', link_id=site_post_data['id'])
+        else:
+            messages.error(request, 'Failed updating link data.', extra_tags='alert-danger')
+            # breadcrumbs
+            bcitems = [['/home/', 'Home'], ['/links/', 'Links'], ['/links/'+str(link_id)+'/', link_data.links_name], ['edit', 'Edit']]
+            return render(request, 'page-site-detail-edit.html', {'title': "Edit Sites", 'head': "Edit Sites", 'bcitems': bcitems, 'contacts_data': contacts_data, 'contacts_type': contacts_type, 'site_form': site_form, 'site_id': site_id})
     else:
         try:
             link_id = int(link_id)
