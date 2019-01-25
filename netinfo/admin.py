1b254ab1d47beb085006d5b20cf8d0eecd73d8ea
Apple <apple@TI-QA-MAC.local>
2019-01-25 23:20:31 +0700

    fix add site

--- netinfo/admin.py
+++ netinfo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import sites, contacts, links
+from .models import sites, contacts, links, devices
 
 admin.site.register(sites)
 admin.site.register(contacts)
 admin.site.register(links)
+admin.site.register(devices)
