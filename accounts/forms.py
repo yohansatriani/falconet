5023fcf528e3efae4f4eb69be95aa35ba4e1c5e0
Yohanes Dwihastana <yohansatriani@yahoo.co.id>
2019-03-12 23:20:29 +0700

    remove accoount apps

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'autocomplete':'off','class': 'form-control', 'id': 'username'}))
-
-    password = forms.CharField(
-        max_length=100,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'off','class': 'form-control', 'id': 'password'})) 
--- falconet/settings.py
+++ falconet/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'netinfo',
     'sla',
-    'accounts'
 ]
 
 MIDDLEWARE = [
